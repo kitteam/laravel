ae2d678ad2637573834c9d68b8c93fc2179e56d5
Anton Popov <a.popov@kit.team>
2019-05-28 22:42:42 +0500

    Добавлено отображение уведомлений, стилизация chekbox

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -37,10 +37,10 @@
                 @endif
             </div>
 
-            {{--<div class="choice choice--checkbox">
+            <div class="choice choice--checkbox">
                 <input class="choice__input" id="remember" name="remember" type="checkbox" {{ old('remember') ? 'checked' : '' }}>
                 <label class="choice__label" for="remember">Запомнить</label>
-            </div>--}}
+            </div>
 
             <button class="btn btn--info btn--fullwidth" type="submit">Войти</button>
 
--- resources/views/flash-message.blade.php
+++ resources/views/flash-message.blade.php
@@ -1,39 +1,39 @@
 @if (session('success'))
-<div class="c-alert c-alert--success">
-    <i class="c-alert__icon fa fa-info-circle"></i> {{ session('success') }}
+<div class="alert alert--success">
+    <i class="alert__icon fa fa-info-circle"></i> {{ session('success') }}
 </div>
 @endif
 
 
 @if (session('error'))
-<div class="c-alert c-alert--danger alert fade show">
-    <i class="c-alert__icon fa fa-times-circle"></i> {{ session('error') }}
+<div class="alert alert--danger alert fade show">
+    <i class="alert__icon fa fa-times-circle"></i> {{ session('error') }}
 
-    <button class="c-close" data-dismiss="alert" type="button">&times;</button>
+    <button class="close" data-dismiss="alert" type="button">&times;</button>
 </div>
 @endif
 
 
 @if (session('warning'))
-<div class="c-alert c-alert--warning alert fade show">
-    <i class="c-alert__icon fa fa-exclamation-circle"></i> {{ session('warning') }}
+<div class="alert alert--warning alert fade show">
+    <i class="alert__icon fa fa-exclamation-circle"></i> {{ session('warning') }}
 
-    <button class="c-close" data-dismiss="alert" type="button">&times;</button>
+    <button class="close" data-dismiss="alert" type="button">&times;</button>
 </div>
 @endif
 
 
 @if (session('info'))
-<div class="c-alert c-alert--info">
-    <i class="c-alert__icon fa fa-info-circle"></i> {{ session('info') }}
+<div class="alert alert--info">
+    <i class="alert__icon fa fa-info-circle"></i> {{ session('info') }}
 </div>
 @endif
 
 
 @if ($errors->any())
-<div class="c-alert c-alert--danger alert fade show">
-    <i class="c-alert__icon fa fa-times-circle"></i> Пожалуйста, проверьте правильность заполения данных формы
+<div class="alert alert--danger alert fade show">
+    <i class="alert__icon fa fa-times-circle"></i> Пожалуйста, проверьте правильность заполения данных формы
 
-    <button class="c-close" data-dismiss="alert" type="button">&times;</button>
+    <button class="close" data-dismiss="alert" type="button">&times;</button>
 </div>
 @endif
