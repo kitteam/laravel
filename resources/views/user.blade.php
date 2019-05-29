43c81228a6d70d5162c82cf4ca27182d4dbb0ed9
Anton Popov <a.popov@kit.team>
2019-05-29 07:58:01 +0500

    Добавлене sidebar

---
+++ resources/views/user.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="page__sidebar js-page-sidebar">
+  <div class="sidebar">
+    
+  </div>
+</div>
+<main class="o-page__content">
+
+</main>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,4 @@ Route::get('/', function () {
 
 Auth::routes();
 
-//Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/user', 'UserController@user')->name('user');
