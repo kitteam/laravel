34d57f6bfa39dc17f1c0d8978125bb5734c6358d
Anton Popov <a.popov@kit.team>
2019-06-02 17:48:54 +0500

    Таблица для отображения кратких новостей

---
+++ database/migrations/2019_06_02_140748_create_moments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMomentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('moments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->unsignedInteger('user_id')->comment('Идентификатор пользователя');
+            $table->enum('status', ['info','success','danger','warning','fancy'])->default('info')->comment('Статус');
+            $table->text('moment');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('moments');
+    }
+}
