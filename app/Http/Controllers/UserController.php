f961c105ed7188c9cfa204cea5dfa06c6283653d
Anton Popov <a.popov@kit.team>
2019-06-06 11:46:34 +0500

    Не учитываем сертификат и временно по 8083 порту для одного из серверов

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -38,9 +38,20 @@ class UserController extends Controller
 
     public function hosting($id = false)
     {
+        stream_context_set_default(array(
+            'ssl'                => array(
+            'peer_name'          => 'generic-server',
+            'verify_peer'        => FALSE,
+            'verify_peer_name'   => FALSE,
+            'allow_self_signed'  => TRUE
+        )));
+
         if ($id) {
             if ($userHosting = UserHosting::where('user_id', Auth::user()->id)->find($id)) {
                 $url = 'https://'. $userHosting->server .'/artisan/';
+                if ('vh1.kit.team' == $userHosting->server) {
+                    $url = 'https://'. $userHosting->server .':8083/artisan/';
+                }
                 if ($key = file_get_contents($url .'?rkey='. $userHosting->account)) {
                     $redirect = $url .'?user='. $userHosting->account .'&token='. hash('sha256', $key);
                     return Redirect::away($redirect);
