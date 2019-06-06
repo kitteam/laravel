6f2835c5b56bdcab87bfc7fe0a0d6c457e4f2438
Anton Popov <a.popov@kit.team>
2019-06-06 09:29:32 +0500

    Добавление пакета для взаимодействия с VestaCP. Отображение дополнительной информации в списке хостинг услуг.

--- resources/views/user/hosting.blade.php
+++ resources/views/user/hosting.blade.php
@@ -26,21 +26,37 @@
 
         <tbody>
         @foreach ($collections as $collection)
+
+            @php
+                $vesta = Vesta::server($collection->server)->setUserName($collection->account);
+                $data = $vesta->listUserAccount();
+                $data = current($data);
+                $domains = array_keys($vesta->listWebDomain());
+            @endphp
+
             <tr class="table__row">
                 <td class="table__cell">
                     {{ $collection->hosting->name }}
+                    <small class="block text-mute">{{ $data['WEB_DOMAINS'] }} {{ trans_choice('сайт|сайта|сайтов', $data['WEB_DOMAINS']) }} / {{ is_numeric($data['DISK_QUOTA']) ? round($data['DISK_QUOTA'] / 1024, 2) : '∞' }} Gb</small>
                 </td>
 
                 <td class="table__cell">
                     <div class="media">
                         <div class="media__body">
                             {{ $collection->account }}<span class="text-mute">@</span><span class="text-mute">{{ $collection->server }}</span>
+                            <small class="block text-mute">{{ $data['FNAME'] ." ".$data['LNAME'] }}</small>
                         </div>
                     </div>
                 </td>
 
                 <td class="table__cell">
-
+                    <ul>
+                    @foreach ($domains as $domain)
+                        <li>
+                            <a href="http://{{ $domain }}" target="_blank">{{ $domain }}</a>
+                        </li>
+                    @endforeach
+                    </ul>
                 </td>
 
                 <td class="table__cell">
