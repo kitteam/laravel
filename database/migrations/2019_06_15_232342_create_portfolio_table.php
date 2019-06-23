75935f1775a67cf77e7e82edc616f04dd138c903
Anton Popov <a.popov@kit.team>
2019-06-23 10:36:11 +0500

    Формирование обложки для vk

---
+++ database/migrations/2019_06_15_232342_create_portfolio_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePortfolioTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('portfolio', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->unsignedInteger('user_id')->comment('Идентификатор пользователя');
+            $table->text('text');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('portfolio');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,41 @@ Route::get('/logout', 'Auth\LoginController@logout')->name('logout' );
 Route::get('/user', 'UserController@user')->name('user');
 Route::get('/user/domain', 'UserController@domain')->name('user.domain');
 Route::get('/user/hosting', 'UserController@hosting')->name('user.hosting');
-Route::get('/user/hosting/{id}', 'UserController@hosting')->where('id', '[0-9]+');
+Route::get('/user/hosting/{id}', 'UserController@hosting');
 
-//Route::get('/test', function () { return view('test'); });
+// usage inside a laravel route
+Route::get('/cover-vk', function()
+{
+    $img = Image::make('img/cover-vk.jpg');
+
+    $array = [
+        DB::table('user_domains')->count(),
+        DB::table('user_hostings')->count(),
+        DB::table('portfolio')->count(),
+    ];
+
+    $img->text($array[0], 784, 99, $font = function($font) {
+        $font->file('fonts/OpenSans-ExtraBold.ttf');
+        $font->size(44);
+        $font->color('#ffffff');
+        $font->align('left');
+        $font->valign('top');
+    });
+    $img->text($array[1], 784, 181, $font);
+    $img->text($array[2], 784, 260, $font);
+
+    $text = trans_choice("Зарегистрированный\nдомен|Зарегистрированных\nдомена|Зарегистрированных\nдоменов", $array[0]);
+    $img->text(mb_strtoupper($text), 876, 138, $font = function($font) {
+        $font->file('fonts/RobotoSlab-Regular.ttf');
+        $font->size(18);
+        $font->color('#ffffff');
+        $font->align('left');
+        $font->valign('bottom');
+    });
+    $text = trans_choice("Активный\nхостинг аккаунт|Активных\nхостинг аккаунта|Активных\nхостинг аккаунтов", $array[1]);
+    $img->text(mb_strtoupper($text), 876, 216, $font);
+    $text = trans_choice("Разработанный\nсайт|Разработанных\nсайта|Разработанных\nсайтов", $array[2]);
+    $img->text(mb_strtoupper($text), 876, 295, $font);
+
+    return $img->response('jpg');
+});
