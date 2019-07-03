19152bcc6aa74474a069e062fb40ab0b2683d1e8
Anton Popov <a.popov@kit.team>
2019-07-03 10:49:57 +0500

    Добавление простой проверки на административный доступ.

---
+++ app/Http/Middleware/Admin.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Auth;
+
+class Admin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (in_array(Auth::user()->id, [1,2,3])) {
+            return $next($request);
+        }
+
+        return route('login');
+    }
+}
