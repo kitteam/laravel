ec09e642022e3095f6d8c31aff4b74495eb9c4f7
Anton Popov <a.popov@kit.team>
2019-07-06 08:50:05 +0500

    Библиотека для взаимодействия с Telegram API

---
+++ config/phptelegrambot.php
@@ -0,0 +1,44 @@
+<?php
+
+return [
+    /**
+     * Bot configuration
+     */
+    'bot'      => [
+        'name'    => env('PHP_TELEGRAM_BOT_NAME', ''),
+        'api_key' => env('PHP_TELEGRAM_BOT_API_KEY', ''),
+    ],
+
+    /**
+     * Database integration
+     */
+    'database' => [
+        'enabled'    => true,
+        'connection' => env('DB_CONNECTION', 'mysql'),
+    ],
+
+    'commands' => [
+        'before'  => true,
+        'paths'   => [
+            // Custom command paths
+        ],
+        'configs' => [
+            // Custom commands configs
+        ],
+    ],
+
+    'admins'  => [
+        // Admin ids
+    ],
+
+    /**
+     * Request limiter
+     */
+    'limiter' => [
+        'enabled'  => false,
+        'interval' => 1,
+    ],
+
+    'upload_path'   => '',
+    'download_path' => '',
+];
