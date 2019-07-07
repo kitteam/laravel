801a864d7af35eb48923b21f52f41c893347304e
Anton Popov <a.popov@kit.team>
2019-07-07 17:53:40 +0500

    Изменение маршрутизации

--- resources/views/control-panel.blade.php
+++ resources/views/control-panel.blade.php
@@ -12,27 +12,27 @@
 <div class="o-page__sidebar js-page-sidebar">
     @section('sidebar')
     <div class="c-sidebar">
-        <a class="c-sidebar__brand" href="{{ route('user.index') }}">
+        <a class="c-sidebar__brand" href="{{ route('cp.index') }}">
             <img class="c-sidebar__brand-img" src="/img/logo-cp.png" alt="logo"> Личный кабинет
         </a>
 
-        @if (Route::has('user.domain'))
-        <h4 class="c-sidebar__title"><a href="{{ route('user.domain') }}">Доменные имена</a></h4>
+        @if (Route::has('cp.domain.list'))
+        <h4 class="c-sidebar__title"><a href="{{ route('cp.domain.list') }}">Доменные имена</a></h4>
         <ul class="c-sidebar__list">
             <li class="c-sidebar__item">
-                <a class="c-sidebar__link {{ route::is('user.domain') ? 'is-active' : '' }}"
-                    href="{{ route('user.domain') }}"><i class="fa u-mr-xsmall"></i> Список доменов
+                <a class="c-sidebar__link {{ route::is('cp.domain.list') ? 'is-active' : '' }}"
+                    href="{{ route('cp.domain.list') }}"><i class="fa u-mr-xsmall"></i> Список доменов
                 </a>
             </li>
         </ul>
         @endif
 
-        @if (Route::has('user.hosting'))
-        <h4 class="c-sidebar__title"><a href="{{ route('user.hosting') }}">Виртуальный хостинг</a></h4>
+        @if (Route::has('cp.hosting.list'))
+        <h4 class="c-sidebar__title"><a href="{{ route('cp.hosting.list') }}">Виртуальный хостинг</a></h4>
         <ul class="c-sidebar__list">
             <li class="c-sidebar__item">
-                <a class="c-sidebar__link {{ route::is('user.hosting') ? 'is-active' : '' }}"
-                    href="{{ route('user.hosting') }}"><i class="fa u-mr-xsmall"></i>Список услуг
+                <a class="c-sidebar__link {{ route::is('cp.hosting.list') ? 'is-active' : '' }}"
+                    href="{{ route('cp.hosting.list') }}"><i class="fa u-mr-xsmall"></i>Список услуг
                 </a>
             </li>
         </ul>
@@ -58,8 +58,8 @@
             </a>
 
             <div class="c-dropdown__menu dropdown-menu dropdown-menu-right" aria-labelledby="dropdwonMenuAvatar">
-                @if (Route::has('user.invoice.refill'))
-                <a class="c-dropdown__item dropdown-item" href="{{ route('user.invoice.refill') }}">Пополнить баланс</a>
+                @if (Route::has('cp.invoice.refill'))
+                <a class="c-dropdown__item dropdown-item" href="{{ route('cp.invoice.refill') }}">Пополнить баланс</a>
                 @endif
                 <a class="c-dropdown__item dropdown-item" href="{{ route('logout') }}">Выход</a>
             </div>
