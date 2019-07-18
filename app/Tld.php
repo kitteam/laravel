b02a015026462b12c06d35a60275224d8478b237
Anton Popov <a.popov@kit.team>
2019-07-18 09:06:40 +0500

    Модель для доменов первого уровня

---
+++ app/Tld.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tld extends Model
+{
+    protected $rate = 3.33;
+
+    public function getRegPriceAttribute($value)
+    {
+        return $value / 100;
+    }
+
+    public function getRetailRegPriceAttribute($value)
+    {
+        return $value / 100;
+    }
+
+    public function getOurRegPriceAttribute()
+    {
+        $difference = $this->retail_reg_price - $this->reg_price;
+        $price = floor($this->reg_price + $difference / $this->rate);
+        return $price > $this->reg_price ? $price : $this->reg_price;
+    }
+
+    public function getRenewPriceAttribute($value)
+    {
+        return $value / 100;
+    }
+
+    public function getRetailRenewPriceAttribute($value)
+    {
+        return $value / 100;
+    }
+
+    public function getOurRenewPriceAttribute()
+    {
+        $difference = $this->retail_renew_price - $this->renew_price;
+        $price = floor($this->renew_price + $difference / $this->rate);
+        return $price > $this->renew_price ? $price : $this->renew_price;
+    }
+
+    public function getUpdateContactsPriceAttribute($value)
+    {
+        return $value / 100;
+    }
+
+    public function getUpdateNameserversPriceAttribute($value)
+    {
+        return $value / 100;
+    }
+}
