ac7d793b024b96f3b277df66b2c384f1b60d9e25
Anton Popov <a.popov@kit.team>
2019-07-30 22:35:08 +0500

    Изменение рейта в большую сторону.

--- app/Tld.php
+++ app/Tld.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tld extends Model
 {
-    protected $rate = 3.33;
+    protected $rate = 2; //3.33;
 
     public function getRegPriceAttribute($value)
     {
